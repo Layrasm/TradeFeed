6bb915a87244f749a0109c0c62858740e1500808
Layne Rasmussen <layrasm@gmail.com>
2021-04-29 00:08:36 -0600

    completed sidebar and styling

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,6 +5,7 @@ import SidebarOption from './SidebarOptions';
 import HomeIcon from '@material-ui/icons/Home';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MailIcon from '@material-ui/icons/Mail';
+import { Button } from '@material-ui/core';
 
 function Sidebar() {
   return (
@@ -14,6 +15,9 @@ function Sidebar() {
       <SidebarOption Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={NotificationsIcon} text="Notifications" />
       <SidebarOption Icon={MailIcon} text="Messages" />
+      <Button variant="outlined" className="sidebarPost" fullWidth>
+        New Post
+      </Button>
 
 
     </div>
